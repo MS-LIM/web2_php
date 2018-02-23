c7aa8654b27489c653290ee0f05091ead3fa2102
lms <acdcefg7@gmail.com>
2018-02-23 22:33:36 +0900

    web2_php 3rd commit add (form, create, update, delete)

--- create.php
+++ create.php
@@ -28,36 +28,32 @@ function print_list(){
 ?>
 <!DOCTYPE html>
 <html>
-  <head>
-    <meta charset="utf-8">
-    <title>
-        <?php
-      print_title();
-      ?>
-    </title>
-  </head>
-  <body>
-    <h1><a href="index.php">WEB</a></h1>
-    <ol>
-        <?php
-        print_list();
-        ?>
-    </ol>
-    <a href="create.php">create</a>
-    <?php if(isset($_GET['id'])) { ?>
-        <a href="update.php?id=<?=$_GET['id']?>">update</a>
-        <form action="delete_process.php" method="post">
-            <input type="hidden" name="id" value="<?=$_GET['id']?>">
-            <input type="submit" value="delete">
-        </form>
-    <?php } ?>
-    <h2>
+    <head>
+        <meta charset="utf-8">
+        <title>
         <?php
         print_title();
         ?>
-    </h2>
+        </title>
+    </head>
+    <body>
+        <h1><a href="index.php">WEB</a></h1>
+        <ol>
         <?php
-        print_description();
+        print_list();
         ?>
-  </body>
+        </ol>
+        <a href="create.php">create</a>
+        <form action="create_process.php" method="post">
+            <p>
+                <input type="text" name="title" placeholder="Title">
+            </p>
+            <p>
+                <textarea name="description" placeholder="Description"></textarea>
+            </p>
+            <p>
+                <input type="submit">
+            </p>
+        </form>
+    </body>
 </html>
